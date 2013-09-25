4a4049c291327081bed4f00c9af1764927db0f90
zonabi <zonabidesign@gmail.com>
2013-09-24 21:15:48 -0500

    implemented URL location routing

--- js/znbdapp.js
+++ js/znbdapp.js
@@ -1,11 +1,22 @@
 var znbdsite = angular.module('znbdsite', ['ngAnimate']);
 
-function bodyControl ($scope) {
-	console.log("bodycontrol");
+function bodyControl ($scope, $location) {
+	
 
+	$scope.location = $location;
+	$scope.$watch('location.search()', function() {
+		$scope.target = ($location.search()).target;
+	}, true);
 
+	$scope.changeTarget = function(name){
+		$location.search('target', name);
+	};
+
+	console.log("bodycontrol location", $scope.location.$$url);
 
 	$scope.currentPage = 'home';
+
+	
 
 	$scope.loadPage = function($page){
 		switch($page){
@@ -30,36 +41,39 @@ function bodyControl ($scope) {
 			default:
 				loadPageHome();
 		}
-		 $("body").animate({scrollTop: top}, "slow");
-	}
+		$("body").animate({scrollTop: top}, "slow");
+	};
 
 	loadPageHome = function(){
 		$scope.currentPage = "home";
-	}
+		$scope.changeTarget("home");
+	};
 	loadPageAbout = function(){
 		$scope.currentPage = "about";
-	}
+		$scope.changeTarget("about");
+	};
 	loadPageWork = function(){
 		console.log("loadPageWork");
 		$scope.currentPage = "work";
-	}
+		$scope.changeTarget("work");
+	};
 	loadPageExp = function(){
 		console.log("loadPageExp");
 		$scope.currentPage = "exp";
-	}
+	};
 	loadPageSocial = function(){
 		console.log("loadPageSoc");
 		$scope.currentPage = "social";
-	}
+	};
 	loadPageContact = function(){
 		console.log("loadPageContact");
 		$scope.currentPage = "contact";
-	}
+	};
 
 	hideAllPages = function(){
 
-	}
+	};
 
-
-
+	$scope.urlSplit = $scope.location.$$url.split("=");
+	$scope.loadPage($scope.urlSplit[1]);
 }
